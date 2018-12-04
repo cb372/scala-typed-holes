78298bc6695fcbcab8fa6bff7ce7de94fd037453
Chris Birchall <chris.birchall@gmail.com>
2018-12-04 20:53:37 +0000

    Show relevant bindings in warning

--- src/main/scala/holes/TypedHolesPlugin.scala
+++ src/main/scala/holes/TypedHolesPlugin.scala
@@ -1,5 +1,6 @@
 package holes
 
+import scala.collection.mutable
 import scala.tools.nsc.{Global, Phase}
 import scala.tools.nsc.ast.TreeDSL
 import scala.tools.nsc.plugins.{Plugin, PluginComponent}
@@ -26,34 +27,73 @@ class TypedHolesComponent(plugin: Plugin, val global: Global)
 
   class TypedHolesTraverser(unit: CompilationUnit) extends Traverser {
 
+    case class Binding(tpe: Type, pos: Position)
+
+    private val bindings: mutable.ArrayStack[Map[TermName, Binding]] = new mutable.ArrayStack
+
     override def traverse(tree: Tree): Unit = {
 
       tree match {
-        case ValDef(_, _, tpt, Hole(rhs)) =>
-          warn(rhs.pos, tpt.tpe)
-        case DefDef(_, _, _, _, tpt, Hole(rhs)) =>
-          warn(rhs.pos, tpt.tpe)
+        case ValDef(_, _, tpt, Hole(holeInRhs)) =>
+          warn(holeInRhs.pos, tpt.tpe)
+          super.traverse(tree)
+        case ValDef(_, _, _, _) =>
+          super.traverse(tree)
+        case DefDef(_, _, _, vparamss, tpt, Hole(holeInRhs)) =>
+          bindings.push(vparamss.flatten.map(param => (param.name, Binding(param.tpt.tpe, param.pos))).toMap)
+          warn(holeInRhs.pos, tpt.tpe)
+          super.traverse(tree)
+          bindings.pop()
+        case DefDef(_, _, _, vparamss, _, _) =>
+          bindings.push(vparamss.flatten.map(param => (param.name, Binding(param.tpt.tpe, param.pos))).toMap)
+          super.traverse(tree)
+          bindings.pop()
         case If(_, Hole(a), Hole(b)) =>
           warn(a.pos, tree.tpe)
           warn(b.pos, tree.tpe)
+          super.traverse(tree)
         case If(_, Hole(a), _) =>
           warn(a.pos, tree.tpe)
+          super.traverse(tree)
         case If(_, _, Hole(b)) =>
           warn(b.pos, tree.tpe)
-        case m@Match(_, cases) =>
+          super.traverse(tree)
+        case m @ Match(_, cases) =>
           cases foreach {
-            case CaseDef(_, _, body) if body.symbol == definitions.Predef_??? =>
-              warn(body.pos, m.tpe)
+            case CaseDef(_, _, Hole(holeInBody)) =>
+              warn(holeInBody.pos, m.tpe)
             case _ =>
           }
+          super.traverse(tree)
         case _ =>
+          super.traverse(tree)
       }
 
-      super.traverse(tree)
     }
 
-    def warn(pos: Position, tpe: Type): Unit =
-      warning(pos, s"Found hole with type $tpe")
+    private def collectRelevantBindings: Map[Name, Binding] =
+      bindings.foldLeft(Map.empty[Name, Binding]){ case (acc, level) => level ++ acc }
+
+    private def warn(pos: Position, tpe: Type): Unit = {
+      val relevantBindingsMessages =
+        collectRelevantBindings.map {
+          case (boundName, Binding(boundType, bindingPos)) => s"  $boundName: $boundType (bound at ${posSummary(bindingPos)})"
+        }
+          .mkString("\n")
+      val message =
+        if (!relevantBindingsMessages.isEmpty)
+          s"""
+             |Found hole with type: $tpe
+             |Relevant bindings include
+             |$relevantBindingsMessages
+           """.stripMargin
+        else
+          s"Found hole with type: $tpe"
+      warning(pos, message)
+    }
+
+    private def posSummary(pos: Position): String =
+      s"${pos.source.file.name}:${pos.line}:${pos.column}"
 
     object Hole {
       def unapply(tree: Tree): Option[Tree] = tree match {
@@ -65,6 +105,7 @@ class TypedHolesComponent(plugin: Plugin, val global: Global)
           None
       }
     }
+
   }
 
 
--- src/test/scala/example/Example.scala
+++ src/test/scala/example/Example.scala
@@ -3,18 +3,43 @@ package example
 object Example {
 
   def foo(x: Int, y: String): Boolean = {
-     if (y.length == x) {
-       ???
-     } else {
-       val umm = "123"
-       ???
-     }
+    if (y.length == x) {
+      ???
+    } else {
+      val umm = "123"
+      ???
+    }
   }
 
   def bar(x: Int): String = x match {
     case 0 => "zero"
-    case 1 => "one"
+    case 1 =>
+      val dunno = "one?"
+      ???
     case _ => ???
+  }
+
+  def polymorphic[A](x: A, y: A): String = {
+    if (x == y) "equal"
+    else ???
+  }
+
+  def parent(x: Int, y: String): Boolean = {
+
+    def child1(x: Int, z: Boolean): Double = {
+      ???
+    }
+
+    def child2(x: Int): String = {
+      if (y.length == x) {
+        ???
+      } else {
+        val umm = "123"
+        ???
+      }
+    }
+
+    child1(123, true) == 0.0
   }
 
   def hole1 = ???
@@ -28,5 +53,5 @@ object Example {
   var hole5 = ???
 
   var hole6: Option[String] = ???
-  
+
 }
