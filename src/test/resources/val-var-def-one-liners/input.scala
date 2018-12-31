d17d5a324a689b242392f4bfa3d5c18cf8fe5196
Chris Birchall <chris.birchall@gmail.com>
2018-12-31 18:16:53 +0000

    Integration test harness
    A framework that runs scalac to compile a single Scala source file with
    the plugin enabled, then checks the compiler output against an expected
    value.
    
    The jar paths for the Scala compiler are passed in from sbt as system
    properties, so `sbt +test` works as expected: the appropriate scalac
    version is used to compile the code.

---
+++ src/test/resources/val-var-def-one-liners/input.scala
@@ -0,0 +1,17 @@
+package foo
+
+object Foo {
+
+  def hole1 = ???
+
+  def hole2: List[String] = ???
+
+  val hole3 = ???
+
+  val hole4: Option[_] = ???
+
+  var hole5 = ???
+
+  var hole6: Option[String] = ???
+
+}
---
+++ src/test/scala/holes/IntegrationTests.scala
@@ -0,0 +1,70 @@
+package holes
+
+import java.nio.charset.StandardCharsets
+import java.nio.file.{Files, Path, Paths}
+
+import org.apache.commons.io.FileUtils
+import org.scalatest.{BeforeAndAfterAll, FunSpec}
+
+import scala.sys.process._
+
+class IntegrationTests extends FunSpec with BeforeAndAfterAll {
+
+  private val pluginJar = sys.props("plugin.jar")
+  private val scalacClasspath = sys.props("scalac.classpath")
+  private val targetDir = Paths.get("target/integration-tests")
+
+  private def runScalac(args: String*): String = {
+    val buf = new StringBuffer
+    val logger = new ProcessLogger {
+      override def out(s: => String): Unit = { buf.append(s); buf.append('\n') }
+      override def err(s: => String): Unit = { buf.append(s); buf.append('\n') }
+      override def buffer[T](f: => T): T = f
+    }
+
+    Process(
+      "java"
+        :: "-Dscala.usejavacp=true"
+        :: "-cp" :: scalacClasspath
+        :: "scala.tools.nsc.Main"
+        :: args.toList
+    ).!(logger)
+
+    buf.toString
+  }
+
+  private def compileFile(path: Path): String =
+    runScalac(
+      s"-Xplugin:$pluginJar",
+      "-P:typed-holes:log-level:info",
+      "-d", targetDir.toString,
+      path.toString
+    )
+
+  override def beforeAll(): Unit = {
+    println(runScalac("-version"))
+
+    FileUtils.deleteQuietly(targetDir.toFile)
+    Files.createDirectories(targetDir)
+  }
+
+  for (scenario <- Paths.get("src/test/resources").toFile.listFiles().toList.map(_.toPath)) {
+    describe(scenario.getFileName.toString) {
+      it("produces the expected output") {
+        val expected =
+          new String(Files.readAllBytes(scenario.resolve("expected.txt")), StandardCharsets.UTF_8).trim
+        val actual =
+          compileFile(scenario.resolve("input.scala")).trim
+
+        if (actual != expected) {
+          println("Compiler output:")
+          println("=====")
+          println(actual)
+          println("=====")
+        }
+        assert(actual === expected)
+      }
+    }
+  }
+
+}
