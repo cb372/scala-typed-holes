d8196a92d54ad5a9d2a0154f712bbb210bc627c5
Chris Birchall <chris.birchall@gmail.com>
2019-01-01 18:18:33 +0000

    Minimise test case

--- src/test/resources/issue-10/input.scala
+++ src/test/resources/issue-10/input.scala
@@ -5,13 +5,10 @@ object CaseApp {
 case class Ops()
 
 object Main {
-  def main(args: Array[String]): Unit =
-    println("Hello, world!")
 
   def parseCmdLine(args: List[String]): Option[Ops] = CaseApp.parse[Ops](args.toSeq) match {
     case Left(_)  => ???
     case Right(_) => ???
   }
 
-  def foo:Int = ???
 }
