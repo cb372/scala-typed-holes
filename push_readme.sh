d712ad3861e3674dd2d1243ecf68f624893d07bc
Chris Birchall <chris.birchall@47deg.com>
2020-06-02 11:46:35 +0100

    Update readme after release

---
+++ push_readme.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#
+# Travis uses this script to commit the updated README.md and push it to GitHub.
+
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+
+git add README.md
+git commit -m '[skip ci] Update version number in README'
+
+git remote add origin-with-token https://${GITHUB_TOKEN}@github.com/cb372/scala-typed-holes.git > /dev/null 2>&1
+
+git push --quiet --set-upstream origin-with-token master
