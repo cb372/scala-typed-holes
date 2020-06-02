58ecdbee4a4e154b4f97638270053661c41eb018
Chris Birchall <chris.birchall@47deg.com>
2020-06-02 17:00:58 +0100

    Fix the git-push script

--- push_readme.sh
+++ push_readme.sh
@@ -2,18 +2,25 @@
 #
 # Travis uses this script to commit the updated README.md and push it to GitHub.
 
-if [ "$(git describe --exact-match --tags master)" == "$TRAVIS_TAG" ]; then
+git fetch origin master
+
+if [ "$(git describe --exact-match --tags FETCH_HEAD)" == "$TRAVIS_TAG" ]; then
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
 
-  git checkout master
+  # Re-clone the repo because Travis clones it with the --branch option,
+  # which means we are on a detached head
+  git clone --depth=50 https://github.com/cb372/scala-typed-holes.git fresh-clone
+
+  cd fresh-clone
+  cp ../README.md .
 
   git add README.md
   git commit -m '[skip ci] Update version number in README'
 
-  git remote add origin-with-token https://${GITHUB_TOKEN}@github.com/cb372/scala-typed-holes.git > /dev/null 2>&1
+  git remote set-url origin https://${GITHUB_TOKEN}@github.com/cb372/scala-typed-holes.git > /dev/null 2>&1
 
-  git push --quiet --set-upstream origin-with-token master
+  git push -u origin master
 else
   echo "A tag was pushed but it was not pointing at the HEAD of master, so I won't push the updated readme to GitHub."
 fi
